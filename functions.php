4db9f4b1d4a86ff4c54a2a2e99465039aa4b09f9
Alex Lesukhin <canek.lis@gmail.com>
2018-09-19 12:30:49 +0300

    module3-task1 Начало шаблонизации

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+
+function count_tasks($project_title, $task_list) {
+    $sum = 0;
+    foreach( $task_list as $task ) {    
+        if ( $task['category'] == $project_title ) {$sum++;}
+    }
+    return $sum;
+}
+
+function include_template($name, $data) {
+    $name = 'templates/' . $name;
+    $result = '';
+
+    if (!file_exists($name)) {
+        return $result;
+    }
+
+    ob_start();
+    extract($data);
+    require_once $name;
+
+    $result = ob_get_clean();
+
+return $result;
+}
