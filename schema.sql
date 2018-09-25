0e8f15284bb16ace56809eb3f2bd68aeea89b203
Alex Lesukhin <canek.lis@gmail.com>
2018-09-25 12:23:26 +0300

    module4-task1 Добавил индесы к полям

--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ USE doingsdone;
  
 CREATE TABLE projects (
     id       INT AUTO_INCREMENT PRIMARY KEY,
-    name     CHAR(255)
+    title     CHAR(255)
 );
 
 CREATE TABLE tasks (  
@@ -23,7 +23,15 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     date_add DATETIME  NOT NULL,
     user_email CHAR(128) NOT NULL,
+    user_name CHAR(128) NOT NULL,
     user_password CHAR(64) NOT NULL,
     user_contacts TEXT,
     UNIQUE INDEX (user_email)
 );
+
+CREATE UNIQUE INDEX emails ON users(user_email);
+CREATE UNIQUE INDEX names ON users(user_name);
+CREATE UNIQUE INDEX projects ON projects(title);
+CREATE UNIQUE INDEX tasks_date_add ON tasks(date_add);
+CREATE UNIQUE INDEX tasks_date_finish ON tasks(date_finish);
+CREATE UNIQUE INDEX tasks_deadline ON tasks(deadline);
