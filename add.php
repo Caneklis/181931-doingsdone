3baab0d53b2b8589063c478a82148fd854534a5c
Alex Lesukhin <canek.lis@gmail.com>
2018-10-09 15:50:25 +0300

    module6-task2 начал работу с валидацией формы

--- add.php
+++ add.php
@@ -9,7 +9,53 @@ require_once ('init.php');
 $link = mysqli_connect('localhost', 'root', '', 'doingsdone');
 mysqli_set_charset($link, "utf8");
 
-if (!$link) {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
+    $tasks = $_POST;
+    
+    $required = ['title', 'description'];
+	$dict = ['title' => 'Название', 'deadline' => 'Срок выполнения', 'project_id' => 'Выбирите проект'];
+    
+	$errors = [];
+	foreach ($required as $key) {
+		if (empty($_POST[$key])) {
+            $errors[$key] = 'Это поле надо заполнить';
+		}
+	}
+    
+    if (count($errors)) {
+        
+        $sql = "SELECT `projects`.`id`, `projects`.`title`, `projects`.`user_id`, COUNT(`tasks`.`id`) AS tasks FROM `projects` LEFT JOIN `tasks` ON `tasks`.`project_id` =`projects`.`id` GROUP BY `projects`.`id`, `projects`.`title`, `projects`.`user_id`";
+
+        if (!$res = mysqli_query($link, $sql)) {
+            $error = mysqli_error($link);
+            $page_content = include_template('error.php', ['error' => $error]);
+        } else {
+            $projects = mysqli_fetch_all($res, MYSQLI_ASSOC);
+        }
+        $where = '';
+
+        if (isset($_GET['project_id'])) {
+            $project_id = mysqli_real_escape_string($link, $_GET['project_id']);
+            $where = " WHERE project_id = " . $project_id;
+        }
+
+        $sql = "SELECT * FROM tasks" . $where;
+        if (!$res = mysqli_query($link, $sql)) {
+            $error = mysqli_error($link);
+            $page_content = include_template('error.php', ['error' => $error]);
+        } else {
+            $tasks = mysqli_fetch_all($res, MYSQLI_ASSOC);
+        }
+        
+		$page_content = include_template('form-task.php', ['tasks' => $tasks, 'errors' => $errors, 'dict' => $dict, "projects" => $projects]);
+        $layout_content = include_template('layout.php', ['projects' => $projects, 'site_title' => $site_title, 'page_content' => $page_content, 'content' => $content]);
+        print($layout_content);
+        //print('Ошибки есть');
+	} else {
+        header("Location: index.php");
+    }
+} else {
+    if (!$link) {
     $error = mysqli_connect_error();
     $page_content = include_template('error.php', ['error' => $error]);
 } else {
@@ -39,14 +85,6 @@ if (!$link) {
 
 $tasks = mysqli_fetch_all(mysqli_query ($link, $sql), MYSQLI_ASSOC);
 
-$page_data = [
-    'projects' => $projects,
-    'tasks' => esc($tasks),
-    'show_complete_tasks' => $show_complete_tasks,
-    'page_content' => $page_content,
-    'content' => $content
-];
-
 $page_content = include_template("form-task.php", ["projects" => $projects]);
 
 $layout_data = [
@@ -59,3 +97,5 @@ $layout_data = [
 $layout_content = include_template('layout.php', $layout_data);
 
 print($layout_content);
+}
+
--- templates/form-task.php
+++ templates/form-task.php
@@ -22,15 +22,20 @@
     <main class="content__main">
         <h2 class="content__main-heading">Добавление задачи</h2>
 
-        <form class="form" action="add.php" method="post">
+        <form class="form" action="add.php" method="post" enctype="multipart/form-data">
             <div class="form__row">
+               <?php $classname = isset($errors['title']) ? "form__input--error" : "";
+                     $value = isset($tasks['title']) ? $tasks['title'] : ""; ?>
+               
                 <label class="form__label" for="name">Название <sup>*</sup></label>
 
-                <input class="form__input" type="text" name="name" id="name" value="" placeholder="Введите название">
+                <input class="form__input <?=$classname;?>" type="text" name="name" id="name" value="<?=$value;?>" placeholder="Введите название">
             </div>
 
             <div class="form__row">
                 <label class="form__label" for="project">Проект <sup>*</sup></label>
+               <?php $classname = isset($errors['project_id']) ? "form__input--error" : "";
+                     $value = isset($tasks['project_id']) ? $tasks['project_id'] : ""; ?>
                 
                 <select class="form__input form__input--select" name="project" id="project">
                 <?php foreach ($projects as $project): ?>
@@ -42,8 +47,10 @@
 
             <div class="form__row">
                 <label class="form__label" for="date">Дата выполнения</label>
+                <?php $classname = isset($errors['deadline']) ? "form__input--error" : "";
+                      $value = isset($tasks['deadline']) ? $tasks['deadline'] : ""; ?>
 
-                <input class="form__input form__input--date" type="date" name="date" id="date" value="" placeholder="Введите дату в формате ДД.ММ.ГГГГ">
+                <input class="form__input form__input--date  <?=$classname;?>" type="date" name="date" id="date" value="<?=$value;?>" placeholder="Введите дату в формате ДД.ММ.ГГГГ">
             </div>
 
             <div class="form__row">
