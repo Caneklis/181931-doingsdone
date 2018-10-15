47f2496358a31e29b4cafe0eea5f16ff640fe303
Alex Lesukhin <canek.lis@gmail.com>
2018-10-15 18:48:17 +0300

    module7-task2 начало авторизации 1

--- auth.php
+++ auth.php
@@ -11,6 +11,7 @@ $link = mysqli_connect('localhost', 'root', '', 'doingsdone');
 mysqli_set_charset($link, "utf8");
 session_start();
 $site_title = "Дела в порядке: Авторезируйтесь";
+$user_id = '';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$form = $_POST;
@@ -22,33 +23,46 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		}
 	}
 
-	$email = mysqli_real_escape_string($link, $form['email']);
-	$sql = "SELECT * FROM users WHERE user_email = '$email'";
-	$res = mysqli_query($link, $sql);
-	$user = $res ? mysqli_fetch_array($res, MYSQLI_ASSOC) : null;
-	if (!count($errors) and $user) {
-		if (password_verify($form['password'], $user['user_password'])) {
-			$_SESSION['user'] = $user;
+	if (!empty($errors)) {
+		$error = mysqli_error($link);
+		$page_content = include_template('add.php', ['error' => $error]);
+	}
+	else {
+		$email = mysqli_real_escape_string($link, $form['email']);
+		$sql = "SELECT * FROM users WHERE user_email = '$email'";
+		$res = mysqli_query($link, $sql);
+		$user = $res ? mysqli_fetch_array($res, MYSQLI_ASSOC) : null;
+		if (!$res = mysqli_query($link, $sql)) {
+			$error = mysqli_error($link);
+			$page_content = include_template('auth.php', ['error' => $error]);
+			$tasks = mysqli_fetch_all($res, MYSQLI_ASSOC);
+		}
+
+		if (!count($errors) and $user) {
+			if (password_verify($form['password'], $user['user_password'])) {
+				$_SESSION['user'] = $user;
+			}
+			else {
+				$errors['password'] = 'Неверный пароль';
+			}
 		}
 		else {
-			$errors['password'] = 'Неверный пароль';
+			$errors['email'] = 'Такой пользователь не найден';
+		}
+
+		if (count($errors)) {
+			$page_content = include_template('auth.php', ['form' => $form, 'errors' => $errors]);
+		}
+		else {
+			header("Location: /index.php");
+			exit();
 		}
 	}
-	else {
-		$errors['email'] = 'Такой пользователь не найден';
-	}
-
-	if (count($errors)) {
-		$page_content = include_template('auth.php', ['form' => $form, 'errors' => $errors]);
-	}
-	else {
-		header("Location: /auth.php");
-		exit();
-	}
 }
+
 else {
 	if (isset($_SESSION['user'])) {
-		$page_content = include_template('index.php', ['user_name' => $_SESSION['user']['user_name'], 'site_title' => $site_title]);
+		$page_content = include_template('index.php', ['user_name' => $_SESSION['user']['user_name'], 'user_id' => $_SESSION['user']['id'], 'site_title' => $site_title]);
 	}
 	else {
 		$page_content = include_template('auth.php', ['site_title' => $site_title]);
