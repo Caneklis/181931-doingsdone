17b802c91ba304d1a6c98f383b1a163f5e33e30d
Alex Lesukhin <canek.lis@gmail.com>
2018-10-15 11:14:59 +0300

    module7-task1 проверка email при регистрации

--- register.php
+++ register.php
@@ -19,35 +19,48 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $required = ["email", "password", "name"];
     $now = date("Y-m-d H:i:s");
     $user_email = mysqli_real_escape_string($link, $_POST["email"]);
-    $passwordHash = password_hash(mysqli_real_escape_string($link, $_POST["password"]), PASSWORD_DEFAULT);
+    $passwordHash = password_hash($_POST["password"], PASSWORD_DEFAULT);
     $user_name = mysqli_real_escape_string($link, $_POST["name"]);
+    $emailClear = $user_email;
 
-      foreach ($required as $key) {
+    foreach ($required as $key) {
         if (empty($_POST[$key])) {
                 $errors[$key] = 'Это поле надо заполнить';
         }
-      }
+    }
+    
+    $sql = mysqli_query($link, "SELECT `id` FROM `users` WHERE `user_email` = '$emailClear'");
+    
+    if (!$sql) {
+        die('Error: ' . mysqli_error($link));
+    }
 
     if (!empty($errors)) {
         $error = mysqli_error($link);
         $page_content = include_template('register.php', ['errors' => $errors]);
 
 	} else {
-    $sql_insert = "INSERT INTO users SET
-        'date_add' = '$now',
-        'user_email' = '$user_email',
-        'user_name' = '$user_name',
-        'user_password' = '$passwordHash' ";
-
-        if (!$res = mysqli_query($link, $sql_insert)) {
-            $error = mysqli_error($link);
-            $page_content = include_template('register.php', ['errors' => $errors]);
-            $users = mysqli_fetch_all($res, MYSQLI_ASSOC);
-        } else {
-            header('Location: /index.php');
+        if(mysqli_num_rows($sql) > 0) {
+            echo "Пользователь с таким email уже зарегестрирован";
         }
-    }
+        else {
+            $sql_insert = "INSERT INTO users SET
+                `date_add` = '$now',
+                `user_email` = '$user_email',
+                `user_name` = '$user_name',
+                `user_password` = '$passwordHash' ";
+
+                if (!$res = mysqli_query($link, $sql_insert)) {
+                    $error = mysqli_error($link);
+                    $page_content = include_template('register.php', ['errors' => $errors]);
+                    $users = mysqli_fetch_all($res, MYSQLI_ASSOC);
+                } else {
+                    header('Location: /index.php');
+                }
+            }
+        }
 }
+
 
 $page_content = include_template("register.php", ['site_title' => $site_title, 'dict' => $dict, 'errors' => $errors,  'users' => $users]);
 
